f66f1ae6a907fca540bc2fdd32aad77d7cdf9939
thibsej <thibault.sejourne@ens.fr>
2021-09-29 15:36:30 +0200

    Update method for greedy init + line search

--- fastuot/tests/test_fastuot.py
+++ fastuot/tests/test_fastuot.py
@@ -1,7 +1,8 @@
 import pytest
 import numpy as np
 from fastuot.uot1d import rescale_potentials, dual_loss, init_greed_uot, \
-    solve_uot
+    solve_uot, compute_shortest_path_support, build_potential_from_support, \
+    lazy_potential
 
 p = 1.5
 
@@ -47,6 +48,19 @@ def test_rescale_potential_increase_score(seed, rho, rho2, mass):
     assert score1 <= score2 + 1e-16
 
 
+@pytest.mark.parametrize('seed', [1, 2, 3, 4, 5, 6, 7])
+def test_lazy_pot_is_feasible(seed):
+    n = int(15)
+    m = int(16)
+    np.random.seed(seed)
+    x = np.sort(np.random.uniform(size=n))
+    y = np.sort(np.random.uniform(size=m))
+    f, g = lazy_potential(x, y, p)
+    T = np.abs(x[:, None] - y[None, :]) ** p + 1e-15 > (
+                f[:, None] + g[None, :])
+    assert np.all(T)
+
+
 @pytest.mark.parametrize('seed,rho,rho2,mass', [(a, b, c, d)
                                                 for a in [1, 2, 3, 4, 5, 6, 7]
                                                 for b in [0.1, 1.0, 10.0]
@@ -90,6 +104,6 @@ def test_pot_fw_is_feasible(seed, rho, rho2, mass, niter):
     _, _, _, f, g, _ = solve_uot(a, b, x, y, p, rho, rho2=rho2, niter=niter,
                                     tol=1e-6,
               greed_init=True, line_search='default', stable_lse=True)
-    T = np.abs(x[:, None] - y[None, :]) ** p + 1e-16 > (
+    T = np.abs(x[:, None] - y[None, :]) ** p + 1e-15 > (
                 ft[:, None] + gt[None, :])
     assert np.all(T)
--- fastuot/uot1d.py
+++ fastuot/uot1d.py
@@ -130,6 +130,8 @@ def dual_loss(f, g, a, b, rho1, rho2=None):
 
 
 def invariant_dual_loss(f, g, a, b, rho1, rho2=None):
+    if rho2 is None:
+        rho2 = rho1
     loss = rho1 * np.sum(a) + rho2 * np.sum(b)
     tau1, tau2 = 1. / (1. + (rho2 / rho1)), 1. / (1. + (rho1 / rho2))
     int_a = np.sum(a * np.exp(-f / rho1))
@@ -179,7 +181,52 @@ def solve_uot(a, b, x, y, p, rho1, rho2=None, niter=100, tol=1e-6,
     return I, J, P, f, g, cost
 
 
-def homogeneous_line_search(fin, gin, fout, gout, a, b, rho1, rho2, nits):
+def pairwise_solve_uot(a, b, x, y, p, rho1, rho2=None, niter=100,
+                       greed_init=True, stable_lse=True):
+    if rho2 is None:
+        rho2 = rho1
+
+    # Initialize potentials
+    if greed_init:
+        f, g = init_greed_uot(a, b, x, y, p, rho1, rho2)
+    else:
+        f, g = np.zeros_like(a), np.zeros_like(b)
+
+    # Store atoms for pairwise steps
+    atoms = [(f, g)]
+
+    for k in range(niter):
+        # Output FW descent direction
+        transl = rescale_potentials(f, g, a, b, rho1, rho2,
+                                    stable_lse=stable_lse)
+        f, g = f + transl, g - transl
+        A, B = a * np.exp(-f / rho1), b * np.exp(-g / rho2)
+        I, J, P, fd, gd, cost = solve_ot(A, B, x, y, p)
+
+        # Find best ascent direction
+        score = np.inf
+        for (ft, gt) in atoms:
+            dscore = np.sum(A * ft) + np.sum(B * gt)
+            if dscore < score:
+                score = dscore
+                fa, ga = ft, gt
+        atoms.append((fd, gd))
+
+        t = 2. / (2. + k)
+        f = f + t * (fd - fa)
+        g = g + t * (gd - ga)
+
+    # Last iter before output
+    transl = rescale_potentials(f, g, a, b, rho1, rho2,
+                                stable_lse=stable_lse)
+    f, g = f + transl, g - transl
+    A, B = a * np.exp(-f / rho1), b * np.exp(-g / rho2)
+    I, J, P, fd, gd, cost = solve_ot(A, B, x, y, p)
+    return I, J, P, f, g, cost
+
+
+def homogeneous_line_search(fin, gin, fout, gout, a, b, rho1, rho2, nits,
+                            tmax=1.):
     """
     Convex interpolation ft = (1 - t) * fin + t * fout.
 
@@ -227,28 +274,25 @@ def homogeneous_line_search(fin, gin, fout, gout, a, b, rho1, rho2, nits):
         t = t - (loss_p / loss_s) / (1 + np.sqrt(loss_p ** 2 / loss_s))
 
         # Clamp to keep a convex combination
-        t = np.maximum(np.minimum(t, 1.), 0.)
+        t = np.maximum(np.minimum(t, tmax), 0.)
     return t
 
 
-def newton_line_search(fin, gin, fout, gout, a, b, rho1, rho2, nits):
+def newton_line_search(fin, gin, d_f, d_g, a, b, rho1, rho2, nits, tmax=1.):
     t = 0.5
     for k in range(nits):
-        ft = (1 - t) * fin + t * fout
-        gt = (1 - t) * gin + t * gout
+        ft = fin + t * d_f
+        gt = gin + t * d_g
         transl = rescale_potentials(ft, gt, a, b, rho1, rho2)
 
-        grad = np.sum(a * (fout - fin) * np.exp(-(ft + transl) / rho1)) \
-               + np.sum(b * (gout - gin) * np.exp(-(gt - transl) / rho2)) \
-            # + 10**(-5-k) / t - 10**(-5-k) / (1-t)
+        grad = np.sum(a * d_f * np.exp(-(ft + transl) / rho1)) \
+               + np.sum(b * d_g * np.exp(-(gt - transl) / rho2))
         hess = -np.sum(
-            a * (fout - fin) ** 2 * np.exp(-(ft + transl) / rho1)) / rho1 \
+            a * d_f ** 2 * np.exp(-(ft + transl) / rho1)) / rho1 \
                - np.sum(
-            b * (gout - gin) ** 2 * np.exp(-(gt - transl) / rho2)) / rho2 \
-            # - 10**(-5-k) / t**2 - 10**(-5-k) / (1-t)**2
+            b * d_g ** 2 * np.exp(-(gt - transl) / rho2)) / rho2
         t = t + (1 / (1 + np.abs(hess))) * (grad / hess)
-        # assert (t>0.) and (t<1.)
-    t = np.maximum(np.minimum(t, 1.), 0.)
+        t = np.maximum(np.minimum(t, tmax), 0.)
     return t
 
 
@@ -260,63 +304,27 @@ def init_greed_uot(a, b, x, y, p, rho1, rho2=None):
     if rho2 is None:
         rho2 = rho1
 
-    # Compute balanced potentials
     _, _, _, fb, gb, _ = solve_ot(a / np.sum(a), b / np.sum(b), x, y, p)
+    fc, gc = lazy_potential(x, y, p)
 
-    # # Compute lazy potential
-    # def cost(i, j):
-    #     return np.abs(x[i] - y[j]) ** p
-    #
-    # spt = compute_shortest_path_support(cost, x.shape[0], y.shape[0])
-    # fc, gc = build_potential_from_support(spt, a, x, b, y, rho1, rho2, p)
-
-    # # Output best convex combination
-    # t = homogeneous_line_search(fb, gb, fc, gc, a, b, rho1, rho2, nits=3)
-    # ft = (1 - t) * fb + t * fc
-    # gt = (1 - t) * gb + t * gc
-    return fb, gb
+    # Output best convex combination
+    t = homogeneous_line_search(fb, gb, fc - fb, gc - gb, a, b, rho1, rho2,
+                                nits=3)
+    ft = (1 - t) * fb + t * fc
+    gt = (1 - t) * gb + t * gc
+    return ft, gt
 
 
-def compute_shortest_path_support(func, n, m):
-    spt = []
-    i, j = 0, 0
-    while (i < n - 1) and (j < m - 1):
-        c12 = func(i, j + 1)
-        c21 = func(i + 1, j)
-        c22 = func(i + 1, j + 1)
-        if (c22 < c12) and (c22 < c21):
-            i = i + 1
-            j = j + 1
-            spt.append((1, 1))
-        elif c12 < c21:
-            j = j + 1
-            spt.append((0, 1))
-        else:
-            i = i + 1
-            spt.append((1, 0))
+@jit(nopython=True)
+def lazy_potential(x, y, p):
+    """Computes the 1D Optimal Transport between two histograms.
 
-    if i == n - 1:
-        while j < m - 1:
-            j = j + 1
-            spt.append((0, 1))
+    _Important: one should have np.sum(a)=np.sum(b)._
 
-    if j == m - 1:
-        while i < n - 1:
-            i = i + 1
-            spt.append((1, 0))
-    return spt
-
-
-def build_potential_from_support(spt, a, x, b, y, p, rho1, rho2):
-    """
-    Assuming the optimal support of the transport plan to be known, computes
-    the associated optimal dual potentials.
+    _Important:_ x and y needs to be sorted.
 
     Parameters
     ----------
-    spt: list of pairs of int, of form (1,0), (0,1) or (1,1).
-    Describes the moves in coordinates in the transport plan's support.
-
     a: vector of length n with positive entries
 
     b: vector of length m with positive entries
@@ -329,48 +337,43 @@ def build_potential_from_support(spt, a, x, b, y, p, rho1, rho2):
 
 
     Returns
-    -------
+    ----------
+    I: vector of length q=n+m-1 of increasing integer in {0,...,n-1}
+
+    J: vector of length q of increasing integer in {0,...,m-1}
+
+    P: vector of length q of positive values of length q
+
     f: dual vector of length n
 
     g: dual vector of length m
+
+    cost: (dual) OT cost
+        sum a_i f_i + sum_j b_j f_j
+        It should be equal to the primal cost
+        = sum_k |x(i)-y(j)|^p where i=I(k), j=J(k)
     """
-    i0, j0 = 0, 0
-    i, j = 0, 0
-    f, g = np.zeros_like(a), np.zeros_like(b)
+    n = x.shape[0]
+    m = y.shape[0]
+    q = m + n - 1
+    i = 0
+    j = 0
+    f = np.zeros(n)
+    g = np.zeros(m)
     g[0] = np.abs(x[0] - y[0]) ** p
-    for (ip, jp) in spt:
-        if (ip == 1) and (jp == 1):
-            i = i + ip
-            j = j + jp
-            # Rescale the finished connected component of support
-            transl = rescale_potentials(f[i0:i], g[j0:j], a[i0:i], b[j0:j],
-                                        rho1, rho2)
-            # Check that translation is admissible
-            transl = check_feasibility(i0, j0, transl, f, g, x, y, p)
-            f[i0:i], g[j0:j] = f[i0:i] + transl, g[j0:j] - transl
-            g[j] = np.abs(x[i] - y[j]) ** p
-            i0, j0 = i, j
-        elif (jp == 0) and (ip == 1):
-            i = i + ip
-            j = j + jp
-            f[i] = np.abs(x[i] - y[j]) ** p - g[j]
-        elif (ip == 0) and (jp == 1):
-            i = i + ip
-            j = j + jp
-            g[j] = np.abs(x[i] - y[j]) ** p - f[i]
-        else:
-            raise Exception
-    transl = rescale_potentials(f[i0:i + 1], g[j0:j + 1], a[i0:i + 1],
-                                b[j0:j + 1], rho1, rho2)
-    transl = check_feasibility(i0, j0, transl, f, g, x, y, p)
-    f[i0:i + 1], g[j0:j + 1] = f[i0:i + 1] + transl, g[j0:j + 1] - transl
+    for k in range(q - 1):
+        c12 = np.abs(x[i] - y[j+1]) ** p
+        c21 = np.abs(x[i + 1] - y[j]) ** p
+        if (c12 < c21) and (i < n - 1):
+            i += 1
+            f[i] = c21 - g[j]
+        elif (c12 > c21) and (j < m - 1):
+            j += 1
+            g[j] = c12 - f[i]
+        elif i == n - 1:
+            j += 1
+            g[j] = c12 - f[i]
+        elif j == m - 1:
+            i += 1
+            f[i] = c21 - g[j]
     return f, g
-
-
-def check_feasibility(i, j, transl, f, g, x, y, p):
-    if (i == 0) or (j == 0):
-        return transl
-    else:
-        gap01 = np.abs(x[i - 1] - y[j]) ** p - f[i - 1] - g[j]
-        gap10 = np.abs(x[i] - y[j - 1]) ** p - f[i] - g[j - 1]
-        return np.minimum(gap10, np.maximum(transl, -gap01))
